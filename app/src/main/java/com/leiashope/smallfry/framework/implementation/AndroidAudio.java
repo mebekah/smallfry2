4e73c6b56370d8c7c1aafe8360c6a73b02e43984
Bekah <bekahrussell@gmail.com>
2015-01-07 20:25:07 -0500

    Initial commit

---
+++ app/src/main/java/com/leiashope/smallfry/framework/implementation/AndroidAudio.java
@@ -0,0 +1,48 @@
+package com.leiashope.smallfry.framework.implementation;
+
+/**
+ * Created by Vader on 1/7/2015.
+ */
+import java.io.IOException;
+
+import android.app.Activity;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.media.AudioManager;
+import android.media.SoundPool;
+
+import com.leiashope.smallfry.framework.Audio;
+import com.leiashope.smallfry.framework.Music;
+import com.leiashope.smallfry.framework.Sound;
+
+public class AndroidAudio implements Audio {
+    AssetManager assets;
+    SoundPool soundPool;
+
+    public AndroidAudio(Activity activity) {
+        activity.setVolumeControlStream(AudioManager.STREAM_MUSIC);
+        this.assets = activity.getAssets();
+        this.soundPool = new SoundPool(20, AudioManager.STREAM_MUSIC, 0);
+    }
+
+    @Override
+    public Music createMusic(String filename) {
+        try {
+            AssetFileDescriptor assetDescriptor = assets.openFd(filename);
+            return new AndroidMusic(assetDescriptor);
+        } catch (IOException e) {
+            throw new RuntimeException("Couldn't load music '" + filename + "'");
+        }
+    }
+
+    @Override
+    public Sound createSound(String filename) {
+        try {
+            AssetFileDescriptor assetDescriptor = assets.openFd(filename);
+            int soundId = soundPool.load(assetDescriptor, 0);
+            return new AndroidSound(soundPool, soundId);
+        } catch (IOException e) {
+            throw new RuntimeException("Couldn't load sound '" + filename + "'");
+        }
+    }
+}
